ca4a6a6065df9b6d63d4914a0486163776c40cd5
dinesh <dinesh.jajoo@aurigait.com>
2025-02-24 12:47:48 +0530

    update config file

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: 'todolist', // Replace with your actual repository name
+  base: '/todolist/', // Replace with your actual repository name
 });
